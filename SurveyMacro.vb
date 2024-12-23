eb5039d9d4ba175f6882b0b774019a220640e357
gvram <gvram@yahoo.com>
2024-12-23 09:12:40 -0500

    Update SurveyMacro.vb with a button and a row

--- SurveyMacro.vb
+++ SurveyMacro.vb
@@ -1,52 +1,50 @@
-Sub ProcessDataFlow()
+Sub GenerateFeedDetails()
     Dim wsInput As Worksheet
     Dim wsOutput As Worksheet
+    Dim wsMain As Worksheet
     Dim lastRow As Long, lastCol As Long
     Dim rowID As Long, colID As Long
     Dim cellValue As String
-    Dim columnName As String
-    Dim outputHeaderRow As Long
-    Dim outputCol As Range
+    Dim feedDescription As String
+    Dim outputLastRow As Long
 
     ' Assign sheets
-    Set wsInput = ThisWorkbook.Sheets(1) ' Input sheet (update if necessary)
+    Set wsInput = ThisWorkbook.Sheets("Sheet1") ' Input sheet
+    Set wsOutput = ThisWorkbook.Sheets("Sheet2") ' Output sheet
     On Error Resume Next
-    Set wsOutput = ThisWorkbook.Sheets("Output")
+    Set wsMain = ThisWorkbook.Sheets("Main") ' Main sheet
     If wsOutput Is Nothing Then
         Set wsOutput = ThisWorkbook.Sheets.Add
-        wsOutput.Name = "Output"
+        wsOutput.Name = "Sheet2"
     End If
     On Error GoTo 0
 
-    ' Define last row and column
+    ' Define last row and column in Input sheet
     lastRow = wsInput.Cells(wsInput.Rows.Count, 1).End(xlUp).Row
     lastCol = wsInput.Cells(1, wsInput.Columns.Count).End(xlToLeft).Column
-    outputHeaderRow = 1
 
-    ' Loop through all cells in the input sheet
+    ' Set headers for the output sheet
+    wsOutput.Cells(1, 1).Value = "Feed Description"
+    wsOutput.Cells(1, 2).Value = "Feed Type"
+    wsOutput.Cells(1, 3).Value = "Data Set Type"
+    wsOutput.Cells(1, 4).Value = "Existing Controls"
+
+    ' Loop through all cells in the Input sheet
     For rowID = 2 To lastRow
         For colID = 2 To lastCol
             cellValue = UCase(wsInput.Cells(rowID, colID).Value)
             If cellValue = "X" Or cellValue = "Y" Or cellValue = "XY" Then
-                ' Create a new column in Output Sheet for Xn or Yn
-                If cellValue Like "*X*" Then
-                    columnName = "X" & colID - 1
-                    Set outputCol = wsOutput.Rows(outputHeaderRow).Find(What:=columnName, LookIn:=xlValues, LookAt:=xlWhole)
-                    If outputCol Is Nothing Then
-                        wsOutput.Cells(outputHeaderRow, wsOutput.Cells(outputHeaderRow, wsOutput.Columns.Count).End(xlToLeft).Column + 1).Value = columnName
-                        Set outputCol = wsOutput.Rows(outputHeaderRow).Find(What:=columnName, LookIn:=xlValues, LookAt:=xlWhole)
-                    End If
-                    wsOutput.Cells(rowID, outputCol.Column).Value = "x"
-                End If
-                If cellValue Like "*Y*" Then
-                    columnName = "Y" & colID - 1
-                    Set outputCol = wsOutput.Rows(outputHeaderRow).Find(What:=columnName, LookIn:=xlValues, LookAt:=xlWhole)
-                    If outputCol Is Nothing Then
-                        wsOutput.Cells(outputHeaderRow, wsOutput.Cells(outputHeaderRow, wsOutput.Columns.Count).End(xlToLeft).Column + 1).Value = columnName
-                        Set outputCol = wsOutput.Rows(outputHeaderRow).Find(What:=columnName, LookIn:=xlValues, LookAt:=xlWhole)
-                    End If
-                    wsOutput.Cells(rowID, outputCol.Column).Value = "y"
-                End If
+                ' Create feed description
+                feedDescription = "Feed from App" & rowID - 1 & " to App" & colID - 1
+
+                ' Find the last row in the output sheet
+                outputLastRow = wsOutput.Cells(wsOutput.Rows.Count, 1).End(xlUp).Row + 1
+
+                ' Write details to the output sheet
+                wsOutput.Cells(outputLastRow, 1).Value = feedDescription
+                wsOutput.Cells(outputLastRow, 2).Value = "Type of Feed" ' Placeholder for Feed Type
+                wsOutput.Cells(outputLastRow, 3).Value = "Type of Data Set" ' Placeholder for Data Set Type
+                wsOutput.Cells(outputLastRow, 4).Value = "Existing Controls" ' Placeholder for Controls
 
                 ' Update original sheet with lowercase values
                 wsInput.Cells(rowID, colID).Value = LCase(cellValue)
@@ -54,5 +52,31 @@ Sub ProcessDataFlow()
         Next colID
     Next rowID
 
-    MsgBox "Data processing completed!", vbInformation
+    MsgBox "Feed details generated in Sheet2!", vbInformation
+End Sub
+
+Sub CreateGenerateButton()
+    Dim wsMain As Worksheet
+    Dim btn As Button
+    Dim btnLeft As Double, btnTop As Double, btnWidth As Double, btnHeight As Double
+
+    ' Assign the Main sheet
+    Set wsMain = ThisWorkbook.Sheets("Main")
+    On Error Resume Next
+    wsMain.Buttons.Delete ' Remove existing buttons if any
+    On Error GoTo 0
+
+    ' Set button dimensions
+    btnLeft = 100
+    btnTop = 50
+    btnWidth = 150
+    btnHeight = 30
+
+    ' Add the button
+    Set btn = wsMain.Buttons.Add(btnLeft, btnTop, btnWidth, btnHeight)
+    btn.OnAction = "GenerateFeedDetails"
+    btn.Caption = "Generate Feeds"
+    btn.Name = "btnGenerateFeeds"
+
+    MsgBox "Generate Button created on the Main sheet!", vbInformation
 End Sub
